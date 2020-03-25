eeacfaea931452c30dd5d025df1a7731ea29466d
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2020-03-25 16:27:27 +0200

    Add user transactions in user account page

--- resources/views/main/users/partials/transactions.blade.php
+++ resources/views/main/users/partials/transactions.blade.php
@@ -5,149 +5,37 @@
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
-                            <th>Payment ID</th>
-                            <th>Amount</th>
-                            <th>Type</th>
-                            <th>Status</th>
-                            <th>Date</th>
+                            <th>كود العملية</th>
+                            <th>نوع العملية</th>
+                            <th>القيمة</th>
+                            <th>وسيلة الدفع</th>
+                            <th>الحالة</th>
+                            <th>تاريخ التنفيذ</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><a href="#">2125</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2126</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2127</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2128</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2129</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2130</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2131</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2132</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2133</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
-                        <tr>
-                            <td><a href="#">2134</a></td>
-                            <td>
-                                <div class="price-amount">
-                                    10
-                                    <span class="currency-symbol">$</span>
-                                </div>
-                            </td>
-                            <td>Direct Bank Transfer</td>
-                            <td>Pending</td>
-                            <td>December 14, 2019 @ 6:49 am</td>
-                        </tr>
+                        @forelse(Auth::user()->transactions as $transaction)
+                            <tr>
+                                <td>{{ $transaction->uid }}</td>
+                                <td>{{ $transaction->type() }}</td>
+                                <td>
+                                    <div class="price-amount">
+                                        {{ $transaction->amount }}
+                                        <span class="currency-symbol">ج م</span>
+                                    </div>
+                                </td>
+                                <td>{{ $transaction->payment_method() }}</td>
+                                <td>{{ $transaction->status() }}</td>
+                                <td>{{ $transaction->created_at->diffForHumans() }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6" class="py-5">لا يوجد أي معاملات مالية حتى الآن</td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
             </div>
-        </div>
-    </div>
-    <div class="pagination-layout1">
-        <div class="btn-prev disabled">
-            <a href="#"><i class="fas fa-angle-double-left"></i>Previous</a>
-        </div>
-        <div class="page-number">
-            <a href="#" class="active">1</a>
-            <a href="#">2</a>
-        </div>
-        <div class="btn-next">
-            <a href="#">Next<i class="fas fa-angle-double-right"></i></a>
         </div>
     </div>
 </div>
