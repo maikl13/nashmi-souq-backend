9d85c2a6fef41b53bc74563d285182cfe35ded99
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2020-08-01 00:25:30 +0200

    Force Https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -2,29 +2,23 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
-use Illuminate\Support\Facades\Auth;
+use App;
 
-class RedirectIfAuthenticated
+class HttpsProtocol
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
+        // redirect to https
         if (!$request->secure() && App::environment() === 'production')
             return redirect()->secure($request->getRequestUri());
-            
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
-        }
-
         return $next($request);
     }
 }
