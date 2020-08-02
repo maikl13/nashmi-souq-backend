7bb11fda1761943d5488065edcc3880a1e48ff9c
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2020-08-02 05:32:32 +0200

    Remove production check for secure link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // if (App::environment() === 'production')
-            // $this->app['request']->server->set('HTTPS', true);
+            $this->app['request']->server->set('HTTPS', true);
     }
 
     /**
