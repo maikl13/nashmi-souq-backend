ea03202444cd9177be799273dad0589b969c9bcb
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2020-08-07 20:17:46 +0200

    Edit Deliver with nashmi section on home page

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -71,13 +71,13 @@
 
     <section class="section-padding-top-heading pt-2 pb-4" style="background: rgb(248,92,112);background: linear-gradient(45deg, rgba(248,92,112,0.6) 0%, rgba(230,92,112,1) 80%);">
         <div class="container text-center">
-            <div class="text-center d-inline-block text-md-right">
-                <div class="p-3 d-inline-block">
+            <div class="text-center d-inline-block text-lg-right">
+                <div class="p-3 d-inline-block" style="vertical-align: middle;">
                     <i class="fa fa-truck text-danger" style="padding: 41px;border-radius: 50%;background: white;font-size: 37px;"></i>
                 </div>
                 <div class="p-1 d-inline-block" style="vertical-align: middle;">
-                    <p class="text-white mb-0" style="font-size: 17px;max-width: 500px;">تقدم لك نشمي خدمة الشحن بأفضل الأسعار, لتتمكن من البيع بدون ان تشغل بالك بأعباء الشحن,.</p>
-                    <a href="/deliver" class="btn btn-default my-3 py-2 px-4" style="background: white;">اشحن مع نشمي</a>
+                    <p class="text-white mb-0 d-inline-block" style="font-size: 17px;max-width: 520px;vertical-align: middle;">تقدم لك نشمي خدمة الشحن بأفضل الأسعار, لتتمكن من البيع بدون ان تشغل بالك بأعباء الشحن,.</p>
+                    <a href="/deliver" class="btn btn-default my-3 py-2 px-4  d-inline-block" style="background: #e65c70;color: white;">اشحن مع نشمي</a>
                 </div>
             </div>
             <div class="clearfix"></div>
