c3509d71d026df8778bd1bea2390053e9252e22a
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-01-31 12:19:07 +0200

    Fix Swap

--- config/swap.php
+++ config/swap.php
@@ -21,7 +21,7 @@ return [
     | * cache_ttl: The cache ttl in seconds.
     */
     'options' => [
-        'cache_ttl' => 432000
+        'cache_ttl' => 86400 // 24 hour
     ],
 
     /*
@@ -72,14 +72,15 @@ return [
     |
     */
     'services' => [
+        'currency_layer' => ['access_key' => '62deca8011b048dc56ed1e4988d31eab', 'enterprise' => false], 
         'fixer' => ['access_key' => 'c112f3482274f1e8f8bd77a584e4e2eb', 'enterprise' => false],
-        // 'fixer' => ['access_key' => 'c14918a163d54cd4446728384f1e4635', 'enterprise' => false],
+        'fixer' => ['access_key' => 'c14918a163d54cd4446728384f1e4635', 'enterprise' => false],
         // 'european_central_bank' => true,
         // 'exchange_rates_api' => true,
         // 'open_exchange_rates' => ['app_id' => '89084bd3770f4bf78e419b5080e5ed1b', 'enterprise' => false],
 
         // 'fixer' => ['access_key' => 'YOUR_KEY', 'enterprise' => false],
-        'currency_layer' => ['access_key' => '62deca8011b048dc56ed1e4988d31eab', 'enterprise' => false],
+        // 'currency_layer' => ['access_key' => 'secret', 'enterprise' => false],],
     ],
 
     /*
