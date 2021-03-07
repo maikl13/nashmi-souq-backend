187d5890516f043842aefc39aa284b886364b4df
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-07 19:11:15 +0200

    Edit footer structure

--- resources/views/main/layouts/partials/footer.blade.php
+++ resources/views/main/layouts/partials/footer.blade.php
@@ -5,7 +5,7 @@
     <div class="footer-top-wrap">
         <div class="container">
             <div class="row">
-                <div class="col-lg-5 col-sm-12">
+                <div class="col-lg-4 col-sm-12">
                     <div class="footer-box-layout1">
                         <div class="footer-logo mb-2">
                             <div class="float-right">
@@ -19,6 +19,33 @@
                         </div>
                         <div class="clearfix"></div>
                         <p>{{ setting('website_description') }}</p>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-sm-8 col-xs-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title mt-4 pb-3">
+                            <h4 class="text-white mb-0">مواقع صديقة</h4>
+                        </div>
+                        <div class="footer-menu-box">
+                            <ul>
+                                @foreach (App\Models\Country::get() as $country)
+                                    <li class="w-auto d-inline-block country-box">
+                                        <a href="/c/{{ $country->code }}" class="px-2 py-1 m-1 d-inline-block" style="border: 1px solid #4a4a4a;">
+                                            <img src="https://flagcdn.com/w40/{{ $country->code }}.png" width="24" style="opacity: .7;" />
+                                            {{-- <img src="https://www.countryflags.io/{{ $country->code }}/shiny/24.png" style="opacity: .5;" /> --}}
+                                            سوق {{ $country->name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-xs-12">
+                    <div class="footer-box-layout1">
+                        <div class="footer-title mt-4 pb-3">
+                            <h4 class="text-white mb-0">قم بمتابعتنا</h4>
+                        </div>
                         <ul class="footer-social">
                             @if( setting('facebook') )
                                 <li><a href="{{ setting('facebook') }}" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
@@ -57,46 +84,17 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-sm-4 col-xs-12">
-                    <div class="footer-box-layout1">
-                        <div class="footer-title">
-                            <h3>معلومات</h3>
-                        </div>
-                        <div class="footer-menu-box">
-                            <ul>
-                                <li><a href="/about">من نحن</a></li>
-                                <li><a href="/terms-and-conditions">شروط الإستخدام</a></li>
-                                <li><a href="/safety-tips">قواعد السلامة</a></li>
-                                <li><a href="/privacy-policy">سياسة الخصوصية</a></li>
-                                <li><a href="/contact-us">إتصل بنا</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5 col-sm-8 col-xs-12">
-                    <div class="footer-box-layout1">
-                        <div class="footer-title">
-                            <h3>مواقع صديقة</h3>
-                        </div>
-                        <div class="footer-menu-box">
-                            <ul>
-                                @foreach (App\Models\Country::get() as $country)
-                                    <li class="w-auto d-inline-block country-box">
-                                        <a href="/c/{{ $country->code }}" class="px-2 py-1 m-1 d-inline-block" style="border: 1px solid #4a4a4a;">
-                                            <img src="https://flagcdn.com/w40/{{ $country->code }}.png" width="24" style="opacity: .7;" />
-                                            {{-- <img src="https://www.countryflags.io/{{ $country->code }}/shiny/24.png" style="opacity: .5;" /> --}}
-                                            سوق {{ $country->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="row">
                 <div class="col mt-3 text-center d-none d-md-block">{!! ad('large_leaderboard') !!}</div>
                 <div class="col mt-3 text-center d-block d-md-none">{!! ad('mobile_banner') !!}</div>
+            </div>
+            <div class="text-center mx-auto mt-4" style="filter: hue-rotate(130deg) brightness(1.5); margin-bottom: -20px;">
+                <a class="text-secondary mx-2" href="/about">من نحن</a>
+                <a class="text-secondary mx-2" href="/terms-and-conditions">شروط الإستخدام</a>
+                <a class="text-secondary mx-2" href="/safety-tips">قواعد السلامة</a>
+                <a class="text-secondary mx-2" href="/privacy-policy">سياسة الخصوصية</a>
+                <a class="text-secondary mx-2" href="/contact-us">إتصل بنا</a>
             </div>
         </div>
     </div>
