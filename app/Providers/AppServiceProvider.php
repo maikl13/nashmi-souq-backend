450f9ba7358d838905eecf34b4fbc1672e3a86d7
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-08 18:56:48 +0200

    Forget old cookies so the new ones will work with all subdomains

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,17 +15,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $current_version = '3.0';
+        // $session_domain = config('session.domain');
+        $current_version = '3.01';
         $version = \Cookie::has('version') ? \Crypt::decryptString(\Cookie::get('version')) : '1.0';
         $version = explode('|', $version);
         $version = end($version);
         if($version != $current_version){
-            foreach ([0,1] as $i) {
-                if($i) \Config::set('session.domain', null);
+            if(request()->path() != '/') {
+                return redirect('/')->send();
+            } else {
+                \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
+                \Config::set('session.domain', null);
                 foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
                     \Cookie::queue(\Cookie::forget($key));
+                // \Config::set('session.domain', $session_domain);
+                // dd(config('session.domain'));
             }
-            \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
+
         }
 
         if (App::environment() === 'production')
