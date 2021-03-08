bee78d470d78eb7c43ed9da3eb3490d56ace5567
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-08 19:09:17 +0200

    Forget old cookies so the new ones will work with all subdomains

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,21 +27,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        // $session_domain = config('session.domain');
-        $current_version = '3.03';
-        $version = \Cookie::has('version') ? \Crypt::decryptString(\Cookie::get('version')) : '1.0';
-        $version = explode('|', $version);
-        $version = end($version);
-        if($version != $current_version){
-            if(request()->path() != '/') {
-                return redirect('/')->send();
-            } else {
-                \Config::set('session.domain', null);
-                foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
-                    \Cookie::queue(\Cookie::forget($key));
-                \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
-                // \Config::set('session.domain', $session_domain);
-                // dd(config('session.domain'));
+        if(request()->getHost() == config('app.url')){
+            $current_version = '3.03';
+            $version = \Cookie::has('version') ? \Crypt::decryptString(\Cookie::get('version')) : '1.0';
+            $version = explode('|', $version);
+            $version = end($version);
+            if($version != $current_version){
+                if(request()->path() != '/') {
+                    return redirect('/')->send();
+                } else {
+                    \Config::set('session.domain', null);
+                    foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
+                        \Cookie::queue(\Cookie::forget($key));
+                    \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
+                }
             }
         }
 
