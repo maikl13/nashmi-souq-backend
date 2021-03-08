30e50c48a1ffc00971f7a4375571606fa7f23d81
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-08 19:01:27 +0200

    Forget old cookies so the new ones will work with all subdomains

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
             if(request()->path() != '/') {
                 return redirect('/')->send();
             } else {
-                \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
                 \Config::set('session.domain', null);
                 foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
                     \Cookie::queue(\Cookie::forget($key));
+                \Cookie::queue(\Cookie::make('version', $current_version, 5*12*30*24*60));
                 // \Config::set('session.domain', $session_domain);
                 // dd(config('session.domain'));
             }
