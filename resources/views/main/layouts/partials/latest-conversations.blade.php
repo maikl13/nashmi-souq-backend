5628489602ca3592960b4208beaa68ce56d40e73
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-15 16:22:29 +0200

    Change chat info to user's data instead of store's

--- resources/views/main/layouts/partials/latest-conversations.blade.php
+++ resources/views/main/layouts/partials/latest-conversations.blade.php
@@ -1,5 +1,5 @@
 @forelse (Auth::user()->unique_conversations()->latest()->orderBy('updated_at', 'desc')->limit(25)->get() as $conversation)
-	<a href="#" class="chat-message clearfix toggle-chat {{ $conversation->messages()->whereNull('seen')->first() ? 'unseen' : '' }}" data-name="{{ $conversation->other_partey()->store_name() }}" data-logo="{{ $conversation->other_partey()->store_logo() }}" data-username="{{ $conversation->other_partey()->username }}">
+	<a href="#" class="chat-message clearfix toggle-chat {{ $conversation->messages()->whereNull('seen')->first() ? 'unseen' : '' }}" data-name="{{ $conversation->other_partey()->name }}" data-logo="{{ $conversation->other_partey()->profile_picture() }}" data-username="{{ $conversation->other_partey()->username }}">
 		<img src="{{ $conversation->other_partey()->profile_picture() }}" class="ml-2" style="width: 50px; height: 50px;">
 		<div class="chat-message-content clearfix">
 			@php
@@ -8,7 +8,7 @@
 			@if ($latest_message)
 				<span class="chat-time">{{ $latest_message->created_at->format('d m Y') == date('d m Y') ? $latest_message->created_at->format('h:i') : $latest_message->created_at->format('d M') }}</span>
 			@endif
-			<h6 class="mb-0">{{ $conversation->other_partey()->store_name() }}</h6>
+			<h6 class="mb-0">{{ $conversation->other_partey()->name }}</h6>
 			@if ($latest_message)
 				<p class="mb-3">
 					<small>{{ Str::limit($latest_message->message, 30, '...') }}</small>
