450f19f7d30e3ea2681dfe89366605ca3abb859a
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-03-15 16:17:48 +0200

    Fix bug - if conversation has no messages for some reason

--- resources/views/main/layouts/partials/latest-conversations.blade.php
+++ resources/views/main/layouts/partials/latest-conversations.blade.php
@@ -2,11 +2,18 @@
 	<a href="#" class="chat-message clearfix toggle-chat {{ $conversation->messages()->whereNull('seen')->first() ? 'unseen' : '' }}" data-name="{{ $conversation->other_partey()->store_name() }}" data-logo="{{ $conversation->other_partey()->store_logo() }}" data-username="{{ $conversation->other_partey()->username }}">
 		<img src="{{ $conversation->other_partey()->profile_picture() }}" class="ml-2" style="width: 50px; height: 50px;">
 		<div class="chat-message-content clearfix">
-			<span class="chat-time">{{ $conversation->messages()->latest()->first()->created_at->format('d m Y') == date('d m Y') ? $conversation->messages()->latest()->first()->created_at->format('h:i') : $conversation->messages()->latest()->first()->created_at->format('d M') }}</span>
+			@php
+				$latest_message = $conversation->messages()->latest()->first();
+			@endphp
+			@if ($latest_message)
+				<span class="chat-time">{{ latest_message->created_at->format('d m Y') == date('d m Y') ? $latest_message->created_at->format('h:i') : $latest_message->created_at->format('d M') }}</span>
+			@endif
 			<h6 class="mb-0">{{ $conversation->other_partey()->store_name() }}</h6>
-			<p class="mb-3">
-				<small>{{ Str::limit($conversation->messages()->latest()->first()->message, 30, '...') }}</small>
-			</p>
+			@if ($latest_message)
+				<p class="mb-3">
+					<small>{{ Str::limit($latest_message->message, 30, '...') }}</small>
+				</p>
+			@endif
 		</div>
 	</a>
 @empty
