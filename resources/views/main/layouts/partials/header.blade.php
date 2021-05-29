f28bc15c9a74ca67c4763566ec4dbacc4a4056ba
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-05-29 23:27:08 +0200

    Remove my orders link from main menu

--- resources/views/main/layouts/partials/header.blade.php
+++ resources/views/main/layouts/partials/header.blade.php
@@ -51,7 +51,6 @@
                                         @endif
                                         <a class="dropdown-item" href="/account">إعدادات الحساب</a>
                                         <a class="dropdown-item" href="/account/my-listing">إعلاناتي</a>
-                                        <a class="dropdown-item" href="/my-orders">طلباتي</a>
                                         @if (auth()->check() && auth()->user()->is_store())
                                             <a class="dropdown-item" href="{{ route('store-dashboard', auth()->user()->store_slug) }}">إدارة المتجر</a>
                                         @endif
