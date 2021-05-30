f993f9b37e6960c3d513596391f7e232cf0ef437
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-05-31 00:10:48 +0200

    Change version number to clear old cookies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,11 +28,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if(request()->getHost() == config('app.domain')){
-            $current_version = '3.07';
+            $current_version = '3.09';
             $version = \Cookie::has('version') ? \Crypt::decryptString(\Cookie::get('version')) : '1.0';
             $version = explode('|', $version);
             $version = end($version);
             if($version != $current_version){
+                foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
+                    \Cookie::queue(\Cookie::forget($key));
                 \Config::set('session.domain', null);
                 foreach (['country','country_code','nashmi_souq_session','XSRF-TOKEN','session_locale','remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d'] as $key)
                     \Cookie::queue(\Cookie::forget($key));
