7e8ddbc983e82799e62c8e8e74ebb0a03c25e417
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-07-09 22:47:18 +0200

    Edit related products links style

--- resources/views/store/products/product.blade.php
+++ resources/views/store/products/product.blade.php
@@ -154,7 +154,7 @@
                                                     <div class="card-body">
                                                         <strong>إختيارات أخرى : </strong> <br>
                                                         @foreach ($related_ps as $i => $p)
-                                                            <a href="{{ $p->url() }}" class="bg-gray px-2 py-1 rounded-lg d-inline-block ml-1">
+                                                            <a href="{{ $p->url() }}" class="bg-gray px-2 py-1 rounded-lg d-inline-block ml-1 mb-1">
                                                                 {{ $p->title }}
                                                                 @foreach ($p->options['values'] as $oval)
                                                                     @php
