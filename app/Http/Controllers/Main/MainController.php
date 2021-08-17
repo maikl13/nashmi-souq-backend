3bbde1aede155482268823e7eeb46b201379f821
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2021-08-17 11:28:17 +0200

    Exclude fixed listings from recent listings list

--- app/Http/Controllers/Main/MainController.php
+++ app/Http/Controllers/Main/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Main;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class MainController extends Controller
@@ -20,9 +21,10 @@ class MainController extends Controller
         $listings = Listing::localized()->active()->leftJoin('featured_listings', 'listings.id', '=', 'featured_listings.listing_id')
             ->select('listings.*', 
                 'featured_listings.listing_id', 
-                \DB::raw("IF('".now()."' < `featured_listings`.`expired_at` and `featured_listings`.`tier` >= 9, -1, 1) as `featuring_level`"),
+                DB::raw("IF('".now()."' < `featured_listings`.`expired_at` and `featured_listings`.`tier` >= 9, 1, 0) as `listed`"),
+                DB::raw("IF('".now()."' < `featured_listings`.`expired_at` and `featured_listings`.`tier` < 9, 1, 0) as `featured`"),
             )
-            ->orderByRaw('`featuring_level` desc, `id` desc')->paginate(8)->shuffle();
+            ->orderByRaw('`featured` desc, `listed` asc, `id` desc')->paginate(8)->shuffle();
 
         if (request()->ajax())
             return response()->json(view('main.layouts.partials.home-listings', ['listings' => $listings])->render(), 200);
