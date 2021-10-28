1ee4ca01f275c3f685ebec30c982e0025eda44a3
mizo-semicolon <60839957+mizo-semicolon@users.noreply.github.com>
2021-10-28 03:16:01 -0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,52 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('categories', 'CategoryController@index');
+
+Route::get('sub_categories/{id}', 'CategoryController@sub_categories');
+Route::get('categories/{id}/options', 'CategoryController@category_options');
+Route::get('listing_types', 'CategoryController@listing_types');
+Route::get('listing_states', 'CategoryController@listing_states');
+Route::post('listings', 'ListingsController@index');
+Route::post('search_listings', 'ListingsController@search_listings');
+Route::get('related_listings/{id}', 'ListingsController@related_listings');
+Route::get('category_listings/{id}', 'ListingsController@category_listings');
+Route::get('sub_category_listings/{id}', 'ListingsController@sub_category_listings');
+Route::get('brands', 'CategoryController@brands');
+Route::get('category_brands/{id}', 'CategoryController@category_brands');
+Route::get('models_brand/{id}', 'CategoryController@models_brand');
+Route::get('user_listings/{id}', 'ListingsController@user_listings');
+Route::get('country_listings/{code}', 'ListingsController@country_listings');
+Route::get('countries', 'CountryController@index');
+Route::get('areas/{id}', 'CountryController@areas');
+Route::get('settings', 'settingsController@index');
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::group(['middleware'=>'auth:api'],function(){
+    Route::get('my_listings', 'UserController@my_listings');
+    Route::post('update_account', 'UserController@update');
+    Route::post('change_password', 'UserController@change_password');
+     Route::post('add_listing', 'UserController@add_listing');
+     Route::post('delete_listing/{id}', 'UserController@destroy');
+     Route::post('edit_listing/{id}', 'UserController@edit');
+     Route::post('delete_listing_image/{id}', 'UserController@delete_listing_image');
+     Route::post('update_payout_methods', 'UserController@update_payout_methods');
+});
+
+/*
+Route::get('states/{state}/areas', 'StateController@areas');
+
+Route::get('categories/{category}/options', 'CategoryController@category_options');
+Route::get('sub-categories/{sub_category}/options', 'CategoryController@sub_category_options');
+*/
+
+
+// الكود ده مهم للفرونت اند :)
+Route::namespace('\App\Http\Controllers\Admin')->group(function () {
+	Route::get('categories/{category}/sub-categories', 'CategoryController@sub_categories');
+	Route::get('states/{state}/areas', 'StateController@areas');
+
+	Route::get('categories/{category}/options', 'CategoryController@category_options');
+	Route::get('sub-categories/{sub_category}/options', 'CategoryController@sub_category_options');
+});
