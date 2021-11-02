6aac28b7a76f5d2cf06e391a2e90a5aa3e467237
mizo-semicolon <60839957+mizo-semicolon@users.noreply.github.com>
2021-11-03 00:25:25 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -64,13 +64,13 @@ Route::group(['middleware'=>'auth:api'],function(){
     Route::post('delete_store_banner', 'StoreController@delete_store_banner');
     Route::post('delete_store_logo', 'StoreController@delete_store_logo');
     Route::get('store_subscriptions', 'StoreController@store_subscriptions')->middleware('store_api');
-    Route::get('store_products', 'StoreController@store_subscriptions')->middleware('store_api');
+    Route::get('store_products', 'StoreController@store_products')->middleware('store_api');
     Route::get('search_store_products', 'StoreController@search_store_products')->middleware('store_api');
     Route::post('create_store_product', 'StoreController@create_store_product')->middleware('store_api');
     Route::post('edit_store_product/{id}', 'StoreController@edit_store_product')->middleware('store_api');
     Route::get('promotions', 'StoreController@promotions')->middleware('store_api');
     Route::post('store_promotions', 'StoreController@store_promotions')->middleware('store_api');
-    Route::post('delete_promotions', 'StoreController@delete_promotions')->middleware('store_api');
+    Route::post('delete_promotions/{id}', 'StoreController@delete_promotions')->middleware('store_api');
     
 });
 
