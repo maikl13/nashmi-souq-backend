c821880897ae5400350c2bf27104f157bda212c1
mizo-semicolon <60839957+mizo-semicolon@users.noreply.github.com>
2021-11-03 00:23:16 +0200

    Update StoreController.php

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
+use Str;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\OptionValue;
+use App\Models\Promotion;
 
 
 class StoreController extends Controller
@@ -151,6 +156,138 @@ class StoreController extends Controller
         return response()->json(['data'=>'تم الحذف بنجاح'
                                       
                                        ],200);
+    }
+    
+    public function store_subscriptions()
+    {
+         return response()->json(['data'=>auth()->user()->subscriptions()->active()],200);
+        
+    }
+    
+    public function store_products()
+    {
+        $products = Product::query()->where('user_id',auth()->user()->id)->latest()->paginate(15);
+        return response()->json(['data'=>$products],200);
+    }
+    
+    public function search_store_products(Request $request)
+    {
+         $validator = Validator::make($request->all(), [
+             'categories.*' => 'nullable|exists:categories,id',
+            'sub_categories.*' => 'nullable|exists:categories,id',
+             ]);
+      if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors(), 'success' => false], 401);
+        }
+        $categories = empty($request->categories) || $request->categories == [null] ? [] : $request->categories;
+        $sub_categories = empty($request->sub_categories) || $request->sub_categories == [null] ? [] : $request->sub_categories;
+        $products = Product::query()->where('user_id',auth()->user()->id);
+         if($request->q && !empty($request->q)) 
+            $products = $products->search($request->q);
+        if( !empty($categories) || !empty($sub_categories) ){
+            $products = $products->Where(function($query) use ($categories, $sub_categories){
+                $query->whereIn('category_id', $categories)
+                    ->orWhereIn('sub_category_id', $sub_categories);
+            });
+        }
+        $products=$products->latest()->paginate(15);
+        return response()->json(['data'=>$products],200);
+    }
+    
+    public function create_store_product(Request $request)
+    {
+         $validator = Validator::make($request->all(), [
+             'product_title' => 'required|min:2|max:255',
+            'description' => 'required|min:2|max:10000',
+            'category' => 'required|exists:categories,slug',
+    		'sub_category' => 'nullable|exists:categories,slug',
+            'images' => 'nullable',
+            'images.*' => 'image|max:8192',
+            'price' => 'nullable|numeric',
+            'currency' => 'nullable|exists:currencies,id',
+             ]);
+      if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors(), 'success' => false], 401);
+        }
+        
+         $title = $request->product_title;
+        $description = $request->description;
+        $slug = Str::slug($request->product_title);
+        $count = Product::withTrashed()->where('slug', $slug)->count();
+        $slug = $count ? $slug.'-'.uniqid() : $slug;
+        $category = Category::where('slug', $request->category)->first();
+        $sub_category = Category::where('slug', $request->sub_category)->first();
+        $group = ($latest = Product::withTrashed()->orderBy('group', 'desc')->first()) ? $latest->group+1 : 1;
+
+        if($request->units){
+            foreach ($request->units as $unit) {
+                $shown = isset($shown) ? false : true;
+                $product = new Product;
+                $product->title = $title;
+                $product->slug = $slug.'-'.uniqid();
+                $product->description = $description;
+                $product->initial_price = $unit['initial_price'] ?? $request->price;
+                $product->price = isset($unit['price']) && $unit['price'] != null ? $unit['price'] : $product->initial_price;
+                $product->currency_id = $request->currency;
+                $product->user_id = Auth::user()->id;
+                $product->category_id = $category->id;
+                $product->sub_category_id = $sub_category ? $sub_category->id : null;
+                $product->group = $group;
+                $product->shown = $shown;
+
+                $option_values = array_merge($unit['option_values'], $request->option_values);
+                if($option_values){
+                    array_unique($option_values);
+                    if (($key = array_search(null, $option_values)) !== false) unset($option_values[$key]);
+                    $options = [];
+                    foreach(OptionValue::whereIn('id', $option_values)->get() as $option_value){
+                        $options['options'][] = $option_value->option_id;
+                        $options['values'][] = $option_value->id;
+                    }
+                    $product->options = $options;
+                }
+                
+                $product->save();
+                $product->upload_product_images(array_merge($unit['images']??[], $request->images??[]));
+            }
+        } else {
+            $product = new Product;
+            $product->title = $request->product_title;
+            $product->slug = $slug.'-'.uniqid();
+            $product->description = $request->description;
+            $product->initial_price = $request->initial_price;
+            $product->price = isset($request->price) && $request->price != null ? $request->price : $product->initial_price;
+            $product->currency_id = $request->currency;
+            $product->user_id = Auth::user()->id;
+            $product->category_id = $category->id;
+            $product->sub_category_id = $sub_category ? $sub_category->id : null;
+            $product->group = $group;
+            $product->shown = true;
+            
+            $option_values = $request->option_values;
+            if($option_values){
+                array_unique($option_values);
+                if (($key = array_search(null, $option_values)) !== false) unset($option_values[$key]);
+                $options = [];
+                foreach(OptionValue::whereIn('id', $option_values)->get() as $option_value){
+                    $options['options'][] = $option_value->option_id;
+                    $options['values'][] = $option_value->id;
+                }
+                $product->options = $options;
+            }
+            
+            $product->save();
+            $product->upload_product_images($request->images);
+        }
+       
+        //return response()->json(['data'=>$products],200);
+    }
+    
+    
+    public function promotions(){
+        $promotions=Promotion::latest()->paginate(15);
+        return response()->json(['data'=>$promotions],200);
+        
     }
       
     
