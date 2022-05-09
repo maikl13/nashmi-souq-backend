b0fcadc1b48f93a7c486aa327a154c3cd354d679
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2022-05-09 20:11:41 +0200

    Edit mobile categories section styling

--- resources/views/main/layouts/partials/categories-mobile.blade.php
+++ resources/views/main/layouts/partials/categories-mobile.blade.php
@@ -1,13 +1,11 @@
 @if(App\Models\Category::whereNull('category_id')->count() && (request()->page && request()->page%3 == 0) )
-    <section class="mt-1 mb-3 py-3 px-2 d-flex align-items-center d-lg-none" style="overflow-x: auto; background: #fefeff; box-shadow: 0 0 12px 3px #fefeff; width: 100%;">
-        {{-- <span class="text-muted small" style="flex-shrink: 0;">المزيد :</span>  --}}
-
-        @foreach(App\Models\Category::whereNull('category_id')->limit(10)->inRandomOrder()->get() as $category)
+    <section class="mt-3 mb-4 py-3 px-2 d-flex align-items-center d-lg-none" style="overflow-x: auto; background: #fff; box-shadow: 0 0 13px 7px #fff; width: 100%;">
+        @foreach(App\Models\Category::whereNull('category_id')->limit(7)->inRandomOrder()->get() as $category)
             <a href="/listings?categories[]={{ $category->id }}" style="font-size: 1.1rem;">
-                <div class="badge text-muted mx-1 py-4 px-3 shadow-sm" style="font-weight: normal; background: #f5f7fa;">
-                    <i class="d-block {{ $category->icon }}"></i>
-                    <span class="d-block mt-3 mb-2">{{ $category->name }}</span>
-                    <div class="item-count small">{{ $category->listings()->count() * 43 }} إعلان</div>
+                <div class="badge text-muted mx-2 py-4 px-3" style="font-weight: normal; background: #f5f7fa; min-width: 150px;">
+                    <i class="d-block text-red {{ $category->icon }}" style="font-size: 22px;"></i>
+                    <span class="d-block mt-3 mb-3" style="font-size: 15px;">{{ $category->name }}</span>
+                    <div class="item-count small">{{ $category->listings()->count() * 23 }} إعلان</div>
                 </div>
             </a>
         @endforeach
