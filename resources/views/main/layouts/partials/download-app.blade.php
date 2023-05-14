558f84570f855cec91e6bc616cfe877078461479
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2023-05-14 20:59:42 +0300

    Change application download url

--- resources/views/main/layouts/partials/download-app.blade.php
+++ resources/views/main/layouts/partials/download-app.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="d-flex justify-content-between mx-auto align-items-center" style="max-width: 700px;">
             <p class="mb-0" style="font-size: .96rem;">تطبيق سوق نشمي للهواتف الذكية، تجربة أفضل و أكثر سهولة</p>
-            <a href="http://onelink.to/sznmf2" class="btn px-1 py-3 text-white bg-red mr-2 d-flex align-items-center justify-content-center" style="width: 190px; max-width: 500px; max-height: 45px; font-size: .95rem;">تحميل التطبيق</a>
+            <a href="https://onelink.to/kf7mqe" class="btn px-1 py-3 text-white bg-red mr-2 d-flex align-items-center justify-content-center" style="width: 190px; max-width: 500px; max-height: 45px; font-size: .95rem;">تحميل التطبيق</a>
         </div>
     </div>
 </div>
